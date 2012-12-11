b90e9e61a8b7612ae60cb31804dbdedeced17707
codeEmitter <greg.johnson@versionone.com>
2012-12-11 15:20:01 -0500

    WIP - Refactoring sample unit tests.  Adding coverage.

--- src/main/java/com/versionone/apiclient/example/DataExamplesTester.java
+++ src/main/java/com/versionone/apiclient/example/DataExamplesTester.java
@@ -3,35 +3,66 @@ package com.versionone.apiclient.example;
 import java.util.Date;
 
 import com.versionone.apiclient.*;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import com.versionone.Oid;
 
 public class DataExamplesTester {
-    private DataExamples docExamples;
+
+    private DataExamples _docExamples;
+    private String _v1Url = "https://www14.v1host.com/v1sdktesting/";
+    private String _user = "admin";
+    private String _password = "admin";
 
     @Before
     public void SetUp() {
-        docExamples = new DataExamples();
+        _docExamples = new DataExamples(_v1Url, _user, _password);
+    }
+
+    @Test
+    public void TestStoryAndDefectTrackingLevel() throws Exception {
+        _docExamples.StoryAndDefectTrackingLevel();
+    }
+
+    @Test
+    public void TestHistoryAsOfTime() throws Exception {
+        Asset[] assets = _docExamples.HistoryAsOfTime();
+        Assert.assertNotNull(assets);
+        Assert.assertTrue(assets.length > 0);
+    }
+
+    @Test
+    public void TestFindListOfAssets() throws Exception {
+        Asset[] assets = _docExamples.FindListOfAssets();
+        Assert.assertNotNull(assets);
+        Assert.assertTrue(assets.length > 0);
+    }
+
+    @Test
+    public void TestSetUpServicesV1Authentication() throws Exception{
+        _docExamples.SetUpServicesV1Authentication();
+    }
+
+    @Test
+    public void TestSetUpServicesWindowsAuthenticationLoggedInUser() throws Exception{
+        _docExamples.SetUpServicesWindowsAuthenticationLoggedInUser();
     }
 
     @Test
     public void TestIsEffortTrackingEnabled() throws Exception{
-        Assert.assertFalse(docExamples.IsEffortTrackingEnabled());
+        Assert.assertFalse(_docExamples.IsEffortTrackingEnabled());
     }
 
     @Test
     public void TestSingleAsset() throws Exception {
-        Asset member = docExamples.SingleAsset();
+        Asset member = _docExamples.SingleAsset();
         Assert.assertNotNull(member);
         Assert.assertEquals("Member:20", member.getOid().getToken());
     }
 
     @Test
     public void TestSingleAssetWithAttributes() throws Exception {
-        Asset member = docExamples.SingleAssetWithAttributes();
+        Asset member = _docExamples.SingleAssetWithAttributes();
         Assert.assertNotNull(member);
         Assert.assertEquals("Member:20", member.getOid().getToken());
         Assert.assertEquals(2, member.getAttributes().size());
@@ -39,63 +70,63 @@ public class DataExamplesTester {
 
     @Test
     public void TestListOfAssets() throws Exception {
-        Asset[] stories = docExamples.ListOfAssets();
+        Asset[] stories = _docExamples.ListOfAssets();
         Assert.assertNotNull(stories);
         Assert.assertTrue(stories.length > 0);
     }
 
     @Test
     public void TestFilterListOfAssets() throws Exception {
-        Asset[] tasks = docExamples.FilterListOfAssets();
+        Asset[] tasks = _docExamples.FilterListOfAssets();
         Assert.assertNotNull(tasks);
         Assert.assertTrue(tasks.length > 0);
     }
 
     @Test
     public void TestSortListOfAssets() throws Exception {
-        Asset[] stories = docExamples.SortListOfAssets();
+        Asset[] stories = _docExamples.SortListOfAssets();
         Assert.assertNotNull(stories);
         Assert.assertTrue(stories.length > 0);
     }
 
     @Test
     public void TestPageListOfAssets() throws Exception {
-        Asset[] stories = docExamples.PageListOfAssets();
+        Asset[] stories = _docExamples.PageListOfAssets();
         Assert.assertNotNull(stories);
         Assert.assertEquals(3, stories.length);
     }
 
     @Test
     public void TestHistorySingleAsset() throws Exception {
-        Asset[] members = docExamples.HistorySingleAsset();
+        Asset[] members = _docExamples.HistorySingleAsset();
         Assert.assertNotNull(members);
         Assert.assertTrue(members.length > 0);
     }
 
     @Test
     public void TestHistoryListOfAssets() throws Exception {
-        Asset[] members = docExamples.HistoryListOfAssets();
+        Asset[] members = _docExamples.HistoryListOfAssets();
         Assert.assertNotNull(members);
         Assert.assertTrue(members.length > 0);
     }
 
     @Test
     public void TestGetV1Configuration() throws Exception {
-        docExamples.GetV1configuration();
+        _docExamples.getV1configuration();
     }
 
 
 
     @Test
     public void TestUpdateScalarAttribute() throws Exception {
-        Asset story = docExamples.UpdateScalarAttribute();
+        Asset story = _docExamples.UpdateScalarAttribute();
         Assert.assertNotNull(story);
 
-        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), docExamples.getMetaModel()));
-        IAssetType storyType = docExamples.getMetaModel().getAssetType("Story");
+        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), _docExamples.getMetaModel()));
+        IAssetType storyType = _docExamples.getMetaModel().getAssetType("Story");
         IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
         query.getSelection().add(nameAttribute);
-        QueryResult result = docExamples.getServices().retrieve(query);
+        QueryResult result = _docExamples.getServices().retrieve(query);
         Asset actualStory = result.getAssets()[0];
 
         Assert.assertEquals(story.getAttribute(nameAttribute).getValue(), actualStory.getAttribute(nameAttribute).getValue());
@@ -103,14 +134,14 @@ public class DataExamplesTester {
 
     @Test
     public void TestUpdateSingleValueRelation() throws Exception {
-        Asset story = docExamples.UpdateSingleValueRelation();
+        Asset story = _docExamples.UpdateSingleValueRelation();
         Assert.assertNotNull(story);
 
-        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), docExamples.getMetaModel()));
-        IAssetType storyType = docExamples.getMetaModel().getAssetType("Story");
+        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), _docExamples.getMetaModel()));
+        IAssetType storyType = _docExamples.getMetaModel().getAssetType("Story");
         IAttributeDefinition sourceAttribute = storyType.getAttributeDefinition("Source");
         query.getSelection().add(sourceAttribute);
-        QueryResult result = docExamples.getServices().retrieve(query);
+        QueryResult result = _docExamples.getServices().retrieve(query);
         Asset actualStory = result.getAssets()[0];
         String expectedSource = story.getAttribute(sourceAttribute).getValue().toString();
         String actualSource = actualStory.getAttribute(sourceAttribute).getValue().toString();
@@ -119,14 +150,14 @@ public class DataExamplesTester {
 
     @Test
     public void TestUpdateMultiValueRelation() throws Exception {
-        Asset story = docExamples.UpdateMultiValueRelation();
+        Asset story = _docExamples.UpdateMultiValueRelation();
         Assert.assertNotNull(story);
 
-        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), docExamples.getMetaModel()));
-        IAssetType storyType = docExamples.getMetaModel().getAssetType("Story");
+        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), _docExamples.getMetaModel()));
+        IAssetType storyType = _docExamples.getMetaModel().getAssetType("Story");
         IAttributeDefinition ownersAttribute = storyType.getAttributeDefinition("Owners");
         query.getSelection().add(ownersAttribute);
-        QueryResult result = docExamples.getServices().retrieve(query);
+        QueryResult result = _docExamples.getServices().retrieve(query);
         Asset actualStory = result.getAssets()[0];
         String expectedOwners = story.getAttribute(ownersAttribute).getValues()[0].toString();
         String actualOwners = actualStory.getAttribute(ownersAttribute).getValues()[0].toString();
@@ -135,14 +166,14 @@ public class DataExamplesTester {
 
     @Test
     public void TestAddNewAsset() throws Exception {
-        Asset story = docExamples.AddNewAsset();
+        Asset story = _docExamples.AddNewAsset();
         Assert.assertNotNull(story);
 
-        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), docExamples.getMetaModel()));
-        IAssetType storyType = docExamples.getMetaModel().getAssetType("Story");
+        Query query = new Query(Oid.fromToken(story.getOid().getMomentless().getToken(), _docExamples.getMetaModel()));
+        IAssetType storyType = _docExamples.getMetaModel().getAssetType("Story");
         IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
         query.getSelection().add(nameAttribute);
-        QueryResult result = docExamples.getServices().retrieve(query);
+        QueryResult result = _docExamples.getServices().retrieve(query);
         Asset actualStory = result.getAssets()[0];
         Assert.assertNotNull(actualStory);
         Assert.assertEquals(story.getAttribute(nameAttribute).getValue(), actualStory.getAttribute(nameAttribute).getValue());
@@ -150,8 +181,8 @@ public class DataExamplesTester {
 
     @Test
     public void TestDeleteAsset() throws Exception {
-        Oid deletedStoryID = docExamples.DeleteAsset();
-        IAssetType storyType = docExamples.getMetaModel().getAssetType("Story");
+        Oid deletedStoryID = _docExamples.DeleteAsset();
+        IAssetType storyType = _docExamples.getMetaModel().getAssetType("Story");
         Query query = new Query(storyType);
         IAttributeDefinition assetState = storyType.getAttributeDefinition("AssetState");
         IAttributeDefinition idAttribute = storyType.getAttributeDefinition("ID");
@@ -165,7 +196,7 @@ public class DataExamplesTester {
         isDeletedTerm.equal(true);
         query.setFilter(new AndFilterTerm(idTerm, isDeletedTerm));
 
-        QueryResult result = docExamples.getServices().retrieve(query);
+        QueryResult result = _docExamples.getServices().retrieve(query);
         Asset deletedStory = result.getAssets()[0];
         Assert.assertNotNull(deletedStory);
         Assert.assertEquals(AssetState.Deleted.value(), deletedStory.getAttribute(assetState).getValue());
@@ -173,15 +204,15 @@ public class DataExamplesTester {
 
     @Test
     public void TestInactivateAsset() throws Exception {
-        Asset inactiveStory = docExamples.CloseAsset();
-        IAttributeDefinition assetState = docExamples.getMetaModel().getAttributeDefinition("Story.AssetState");
+        Asset inactiveStory = _docExamples.CloseAsset();
+        IAttributeDefinition assetState = _docExamples.getMetaModel().getAttributeDefinition("Story.AssetState");
         Assert.assertEquals(AssetState.Closed.value(), inactiveStory.getAttribute(assetState).getValue());
     }
 
     @Test
     public void TestReOpenAsset() throws Exception {
-        Asset activeStory = docExamples.ReOpenAsset();
-        IAttributeDefinition assetState = docExamples.getMetaModel().getAttributeDefinition("Story.AssetState");
+        Asset activeStory = _docExamples.ReOpenAsset();
+        IAttributeDefinition assetState = _docExamples.getMetaModel().getAttributeDefinition("Story.AssetState");
         Assert.assertEquals(AssetState.Active.value(), activeStory.getAttribute(assetState).getValue());
     }
 
@@ -189,12 +220,12 @@ public class DataExamplesTester {
     public void createStoryThroughProxyTest() throws Exception {
         String storyName = "proxy story test " + new Date();
         String projectToken = "Scope:0";
-        Asset story = docExamples.createStoryThroughProxy(storyName, projectToken);
+        Asset story = _docExamples.createStoryThroughProxy(storyName, projectToken);
         IAttributeDefinition nameDef = story.getAssetType().getAttributeDefinition("Name");
 
         Assert.assertEquals(storyName, story.getAttribute(nameDef).getValue());
 
-        IAssetType storyType = docExamples.getMetaModel().getAssetType("Story");
+        IAssetType storyType = _docExamples.getMetaModel().getAssetType("Story");
         Query query = new Query(storyType);
         IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
         IAttributeDefinition idAttribute = storyType.getAttributeDefinition("ID");
@@ -203,7 +234,7 @@ public class DataExamplesTester {
         idTerm.equal(story.getOid());
         query.setFilter(idTerm);
 
-        QueryResult result = docExamples.getServices().retrieve(query);
+        QueryResult result = _docExamples.getServices().retrieve(query);
         Asset storyWithoutProxy = result.getAssets()[0];
 
         Assert.assertEquals(storyName, storyWithoutProxy.getAttribute(nameDef).getValue());
