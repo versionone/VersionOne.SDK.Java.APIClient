1db023171f43d283a96747b5d4acada0cfe8eb94
codeEmitter <greg.johnson@versionone.com>
2012-12-13 17:35:42 -0500

    D-07479 Adding concept of EnvironmentContext to unit tests only during design.  Adding missing unit tests for Asset.java.

--- src/test/java/com/versionone/apiclient/tests/IntegrationTests.java
+++ src/test/java/com/versionone/apiclient/tests/IntegrationTests.java
@@ -3,6 +3,7 @@ import com.versionone.Oid;
 import com.versionone.apiclient.*;
 import junit.framework.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class IntegrationTests {
@@ -14,27 +15,28 @@ public class IntegrationTests {
 
     @Before
     public void Setup(){
+        _dataConnector = EnvironmentContext.Connectors.getDataConnector();
+        _metaConnector = EnvironmentContext.Connectors.getMetaConnector();
+        _metaModel = EnvironmentContext.Models.getMetaModel();
+        _services = EnvironmentContext.Services.getServices();
+    }
 
-        _dataConnector = new V1APIConnector(
-                EnvironmentContext.Urls.getDataUrl(),
-                EnvironmentContext.Credentials.getV1UserName(),
-                EnvironmentContext.Credentials.getV1Password()
-                );
+    @Test @Ignore("WIP")
+    public void AddNewAssetWithNonExistentScopeContext() throws Exception {
+        Oid projectId = Oid.fromToken("Scope:999999999", _metaModel);
+        IAssetType assetType = _metaModel.getAssetType("Story");
+        Asset newStory = _services.createNew(assetType, projectId);
+        IAttributeDefinition nameAttribute = assetType.getAttributeDefinition("Name");
+        newStory.setAttributeValue(nameAttribute, "My New Story");
+        _services.save(newStory);
 
-        _metaConnector = new V1APIConnector(
-                EnvironmentContext.Urls.getMetaUrl(),
-                EnvironmentContext.Credentials.getV1UserName(),
-                EnvironmentContext.Credentials.getV1Password()
-                );
-
-        _metaModel = new MetaModel(_metaConnector);
-        _services = new Services(_metaModel, _dataConnector);
-
+        Assert.assertTrue(newStory.getOid().getToken().length() != 0);
+        Assert.assertTrue(newStory.getAttribute(assetType.getAttributeDefinition("Scope")).getValue() != null);
+        Assert.assertTrue(newStory.getAttribute(nameAttribute).getValue() != null);
     }
 
     @Test
     public void AddNewAssetWithExistingScopeContext() throws Exception {
-
         Oid projectId = Oid.fromToken("Scope:0", _metaModel);
         IAssetType assetType = _metaModel.getAssetType("Story");
         Asset newStory = _services.createNew(assetType, projectId);
@@ -45,7 +47,6 @@ public class IntegrationTests {
         Assert.assertTrue(newStory.getOid().getToken().length() != 0);
         Assert.assertTrue(newStory.getAttribute(assetType.getAttributeDefinition("Scope")).getValue() != null);
         Assert.assertTrue(newStory.getAttribute(nameAttribute).getValue() != null);
-
     }
 
 }
