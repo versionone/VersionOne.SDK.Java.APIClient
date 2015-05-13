5e8eed8f46de8e4b1ab21705831b22268e3812d2
Valentin Plechuc <vplechuc@gmail.com>
2015-05-13 16:50:42 -0300

    query and update testing classes

--- src/test/java/com/versionone/sdk/integration/tests/QueryAssets.java
+++ src/test/java/com/versionone/sdk/integration/tests/QueryAssets.java
@@ -1,11 +1,445 @@
 package com.versionone.sdk.integration.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.versionone.DB.DateTime;
+import com.versionone.Oid;
+import com.versionone.apiclient.Asset;
+import com.versionone.apiclient.Attachments;
+import com.versionone.apiclient.Attribute;
+import com.versionone.apiclient.MimeType;
+import com.versionone.apiclient.Paging;
+import com.versionone.apiclient.Query;
+import com.versionone.apiclient.V1Connector;
+import com.versionone.apiclient.exceptions.MetaException;
+import com.versionone.apiclient.exceptions.OidException;
+import com.versionone.apiclient.exceptions.V1Exception;
+import com.versionone.apiclient.filters.AndFilterTerm;
+import com.versionone.apiclient.filters.FilterTerm;
+import com.versionone.apiclient.interfaces.IAssetType;
+import com.versionone.apiclient.interfaces.IAttachments;
+import com.versionone.apiclient.interfaces.IAttributeDefinition;
+import com.versionone.apiclient.interfaces.IServices;
+import com.versionone.apiclient.services.OrderBy;
+import com.versionone.apiclient.services.OrderBy.Order;
+import com.versionone.apiclient.services.QueryFind;
+import com.versionone.apiclient.services.QueryResult;
 
 public class QueryAssets {
 
+	private static IServices _services;
+	private static String _instanceUrl;
+	private static String _accessToken;
+	private static Oid _projectId;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Running attachments integration tests...");
+		_services = APIClientIntegrationTestSuiteIT.get_services();
+		_instanceUrl = APIClientIntegrationTestSuiteIT.get_instanceUrl();
+		_accessToken = APIClientIntegrationTestSuiteIT.get_accessToken();
+		_projectId = APIClientIntegrationTestSuiteIT.get_projectId();
+	}
+
+	@Test(expected = OidException.class)
+	public void QueryInvalidOid() throws OidException {
+
+		Oid invalidOid = _services.getOid("unknown:007");
+	}
+
+	@Test
+	public void querySingleAssetTest() throws V1Exception {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query single asset";
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+
+		Query query = new Query(newStory.getOid());
+		query.getSelection().add(nameAttribute);
+		Asset story = _services.retrieve(query).getAssets()[0];
+
+		assertNotNull(story);
+		assertTrue(story.getAttribute(nameAttribute).getValue().toString().equals(name));
+	}
+
+	@Test(expected = MetaException.class)
+	public void queryUnknownSingleAssetTest() {
+		IAssetType storyType = _services.getMeta().getAssetType("Unknown");
+	}
+
+	@Test
+	public void queryMultipleAssetsTest() throws V1Exception {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query multiple assets";
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		QueryResult result = _services.retrieve(query);
+
+		assertTrue(result.getAssets().length > 2);
+	}
+
+	@Test
+	public void queryAttributesTest() throws V1Exception {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		IAttributeDefinition benefitsAttribute = storyType.getAttributeDefinition("Benefits");
+		IAttributeDefinition estimateAttribute = storyType.getAttributeDefinition("Estimate");
+		newStory.setAttributeValue(nameAttribute, "Test Story on " + _projectId + " - Name attribute");
+		newStory.setAttributeValue(benefitsAttribute, "Test Story on " + _projectId + " - Benefits attribute");
+		newStory.setAttributeValue(estimateAttribute, "24");
+		_services.save(newStory);
+
+		Query query = new Query(newStory.getOid());
+		query.getSelection().add(nameAttribute);
+		query.getSelection().add(benefitsAttribute);
+		query.getSelection().add(estimateAttribute);
+
+		Asset story = _services.retrieve(query).getAssets()[0];
+		assertNotNull(story);
+
+		Attribute nameAttr = story.getAttribute(nameAttribute);
+		assertNotNull(nameAttr);
+		assertTrue(nameAttr.getValue().toString().equals("Test Story on " + _projectId + " - Name attribute"));
+
+		Attribute benefitsAttr = story.getAttribute(benefitsAttribute);
+		assertNotNull(benefitsAttr);
+		assertTrue(benefitsAttr.getValue().toString().equals("Test Story on " + _projectId + " - Benefits attribute"));
+
+		Attribute estimateAttr = story.getAttribute(estimateAttribute);
+		assertNotNull(estimateAttr);
+		assertTrue(estimateAttr.getValue().toString().equals("24"));
+	}
+
+	@Test(expected = MetaException.class)
+	public void queryUnknownAttributeTest() throws V1Exception {
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition queryUnknowAttribute = storyType.getAttributeDefinition("Unknown");
+	}
+
+	@Test
+	public void queryRelationsTest() throws V1Exception {
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query relations";
+		newStory.setAttributeValue(nameAttribute, _projectId);
+		IAssetType workitemPriorityType = _services.getMeta().getAssetType("WorkitemPriority");
+		Asset newWorkitemPriority = _services.createNew(workitemPriorityType, _projectId);
+		newWorkitemPriority.setAttributeValue(workitemPriorityType.getAttributeDefinition("Name"), "Test WorkItemPriority on " + _projectId);
+		_services.save(newWorkitemPriority);
+
+		IAttributeDefinition priorityAttribute = storyType.getAttributeDefinition("Priority");
+		newStory.setAttributeValue(priorityAttribute, newWorkitemPriority.getOid());
+		_services.save(newStory);
+
+		Query query = new Query(newStory.getOid());
+		query.getSelection().add(priorityAttribute);
+		Asset story = _services.retrieve(query).getAssets()[0];
+
+		assertNotNull(story);
+
+		Attribute workitemPriority = story.getAttribute(priorityAttribute);
+		assertNotNull(workitemPriority);
+		assertTrue(workitemPriority.getValue().toString().equals(newWorkitemPriority.getOid().toString()));
+	}
+
+	@Test
+	public void queryFilterTest() throws V1Exception {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query filter";
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, "Another " + name);
+		_services.save(newStory);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		FilterTerm filterTerm = new FilterTerm(nameAttribute);
+		filterTerm.equal(name);
+		query.setFilter(filterTerm);
+		QueryResult result = _services.retrieve(query);
+
+		assertNotNull(result);
+		assertTrue(result.getAssets().length == 2);
+
+		for (Asset asset : result.getAssets()) {
+			assertTrue(asset.getAttribute(nameAttribute).getValue().toString().equals(name));
+		}
+	}
+
+	@Test
+	public void queryFilterWithMultipleAttributesTest() throws V1Exception {
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		IAttributeDefinition estimateAttribute = storyType.getAttributeDefinition("Estimate");
+		String name = "Test Story " + _projectId + " Query filter with multiple attributes";
+		newStory.setAttributeValue(nameAttribute, name);
+		newStory.setAttributeValue(estimateAttribute, "24");
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name);
+		newStory.setAttributeValue(estimateAttribute, "2");
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, "Another " + name);
+		newStory.setAttributeValue(estimateAttribute, "2");
+		_services.save(newStory);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		query.getSelection().add(estimateAttribute);
+		FilterTerm nameFilterTerm = new FilterTerm(nameAttribute);
+		nameFilterTerm.equal(name);
+		FilterTerm estimateFilterTerm = new FilterTerm(estimateAttribute);
+		estimateFilterTerm.equal("24");
+		query.setFilter(new AndFilterTerm(nameFilterTerm, estimateFilterTerm));
+		QueryResult result = _services.retrieve(query);
+
+		assertNotNull(result);
+		assertTrue(result.getAssets().length == 1);
+
+		for (Asset asset : result.getAssets()) {
+			assertTrue(asset.getAttribute(nameAttribute).getValue().toString().equals(name));
+		}
+		for (Asset asset : result.getAssets()) {
+			assertTrue(asset.getAttribute(estimateAttribute).getValue().toString().equals("24"));
+		}
+
+	}
+
+	@Test
+	public void queryFindTest() throws V1Exception, InterruptedException {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		IAttributeDefinition descriptionAttribute = storyType.getAttributeDefinition("Description");
+		String name = "Test Story " + _projectId + " Query find";
+		newStory.setAttributeValue(nameAttribute, name);
+		newStory.setAttributeValue(descriptionAttribute, "Description for " + name);
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, "Another " + name);
+		newStory.setAttributeValue(descriptionAttribute, "Description for " + name);
+		_services.save(newStory);
+
+		Thread.currentThread().sleep(10000);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		query.getSelection().add(descriptionAttribute);
+		query.setFind(new QueryFind(name));
+		QueryResult result = _services.retrieve(query);
+
+		assertNotNull(result);
+		assertTrue(result.getAssets().length == 2);
+
+		for (Asset asset : result.getAssets()) {
+			assertTrue(asset.getAttribute(descriptionAttribute).getValue().toString().toUpperCase().indexOf(name.toUpperCase()) >= 0);
+		}
+
+	}
+
+	@Test(expected = MetaException.class)
+	public void queryFindUnknownAssetTest() {
+		Query query = new Query(_services.getMeta().getAssetType("Unknown"));
+	}
+
+	@Test
+	public void querySortTest() throws V1Exception, InterruptedException {
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query sort";
+		newStory.setAttributeValue(nameAttribute, name + " 2");
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name + " 1");
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name + " 3");
+		_services.save(newStory);
+
+		Thread.currentThread().sleep(10000);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		query.setFind(new QueryFind(name));
+		OrderBy value = null;
+		value.minorSort(nameAttribute, Order.Ascending);
+		query.setOrderBy(value);
+		QueryResult result = _services.retrieve(query);
+
+		assertTrue(result.getAssets().length == 3);
+		assertTrue(result.getAssets()[0].getAttribute(nameAttribute).getValue().toString().endsWith("1"));
+		assertTrue(result.getAssets()[1].getAttribute(nameAttribute).getValue().toString().endsWith("2"));
+		assertTrue(result.getAssets()[2].getAttribute(nameAttribute).getValue().toString().endsWith("3"));
+	}
+
+	@Test
+	public void queryPagingTest() throws V1Exception, InterruptedException {
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query paging";
+		newStory.setAttributeValue(nameAttribute, name + " 1");
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name + " 2");
+		_services.save(newStory);
+		newStory = _services.createNew(storyType, _projectId);
+		newStory.setAttributeValue(nameAttribute, name + " 3");
+		_services.save(newStory);
+
+		Thread.currentThread().sleep(10000);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		query.setFind(new QueryFind(name));
+		query.setPaging(new Paging(0, 2));
+		QueryResult result = _services.retrieve(query);
+
+		assertTrue(result.getAssets().length == 2);
+	}
+
+	@Test
+	public void queryHistoryTest() throws V1Exception {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query history";
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+		newStory.setAttributeValue(nameAttribute, name + " - updated");
+		_services.save(newStory);
+		newStory.setAttributeValue(nameAttribute, name + " - updated again");
+		_services.save(newStory);
+
+		Query query = new Query(storyType, true);
+		query.getSelection().add(nameAttribute);
+		FilterTerm filterTerm = new FilterTerm(storyType.getAttributeDefinition("ID"));
+		filterTerm.equals(newStory.getOid().getMomentless());
+		query.setFilter(filterTerm);
+		QueryResult result = _services.retrieve(query);
+
+		assertNotNull(result);
+		assertTrue(result.getAssets().length == 3);
+	}
+
+	@Test
+	public void queryAsofTest() throws V1Exception, InterruptedException {
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		String name = "Test Story " + _projectId + " Query asof";
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+
+		Thread.currentThread().sleep(10000);
+
+		newStory.setAttributeValue(nameAttribute, name + " - updated");
+		_services.save(newStory);
+		newStory.setAttributeValue(nameAttribute, name + " - updated again");
+		_services.save(newStory);
+
+		Query query = new Query(storyType);
+		query.getSelection().add(nameAttribute);
+		query.setAsOf(DateUtils.addMilliseconds(DateTime.now().getValue(), -3500));
+		QueryResult result = _services.retrieve(query);
+
+		assertNotNull(result);
+		assertEquals(0, result.getTotalAvaliable());
+	}
+
+	@Test
+	public void QueryAttachment() throws V1Exception, IOException {
+		String file = "com/versionone/apiclient/versionone.png";
+		assertNotNull("Test file missing", Thread.currentThread().getContextClassLoader().getResource(file));
+		String mimeType = MimeType.resolve(file);
+
+		IAttachments attachments = new Attachments(V1Connector.withInstanceUrl(_instanceUrl).withUserAgentHeader(".NET_SDK_Integration_Test", "1.0")
+				.withAccessToken(_accessToken).useEndpoint("attachment.img/").build());
+
+		IAssetType storyType = _services.getMeta().getAssetType("Story");
+		Asset newStory = _services.createNew(storyType, _projectId);
+		IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+		IAttributeDefinition attachmentsAttribute = storyType.getAttributeDefinition("Attachments");
+		String name = "Test Story " + _projectId + " Query attachment";
+		newStory.setAttributeValue(nameAttribute, name);
+		_services.save(newStory);
+
+		IAssetType attachmentType = _services.getMeta().getAssetType("Attachment");
+		IAttributeDefinition attachmentAssetDef = attachmentType.getAttributeDefinition("Asset");
+		IAttributeDefinition attachmentContent = attachmentType.getAttributeDefinition("Content");
+		IAttributeDefinition attachmentContentType = attachmentType.getAttributeDefinition("ContentType");
+		IAttributeDefinition attachmentFileName = attachmentType.getAttributeDefinition("Filename");
+		IAttributeDefinition attachmentName = attachmentType.getAttributeDefinition("Name");
+		Asset newAttachment = _services.createNew(attachmentType, Oid.Null);
+		newAttachment.setAttributeValue(attachmentName, "Test Attachment on " + newStory.getOid());
+		newAttachment.setAttributeValue(attachmentFileName, file);
+		newAttachment.setAttributeValue(attachmentContentType, mimeType);
+		newAttachment.setAttributeValue(attachmentContent, "");
+		newAttachment.setAttributeValue(attachmentAssetDef, newStory.getOid());
+		_services.save(newAttachment);
+		String key = newAttachment.getOid().getKey().toString();
+
+		FileInputStream inStream = new FileInputStream(Thread.currentThread().getContextClassLoader().getResource(file).getPath());
+		OutputStream output = attachments.getWriter(key, mimeType);
+		byte[] buffer = new byte[inStream.available() + 1];
+		while (true) {
+			int read = inStream.read(buffer, 0, buffer.length);
+			if (read <= 0)
+				break;
+
+			output.write(buffer, 0, read);
+		}
+
+		attachments.setWriter(key);
+
+		Query query = new Query(attachmentType);
+		query.getSelection().add(attachmentContent);
+		Asset attachment = _services.retrieve(query).getAssets()[0];
+
+		assertNotNull(attachment);
+		assertNotNull(attachment.getAttribute(attachmentContent).getValue());
 	}
 
 }
--- src/test/java/com/versionone/sdk/integration/tests/UpdateAssets.java
+++ src/test/java/com/versionone/sdk/integration/tests/UpdateAssets.java
@@ -1,24 +1,112 @@
 package com.versionone.sdk.integration.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.versionone.Oid;
+import com.versionone.apiclient.Asset;
+import com.versionone.apiclient.Query;
+import com.versionone.apiclient.exceptions.MetaException;
+import com.versionone.apiclient.exceptions.V1Exception;
+import com.versionone.apiclient.interfaces.IAssetType;
+import com.versionone.apiclient.interfaces.IAttributeDefinition;
+import com.versionone.apiclient.interfaces.IServices;
+
 public class UpdateAssets {
+	private static IServices _services;
+	private static Oid _projectId;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Running attachments integration tests...");
+		_services = APIClientIntegrationTestSuiteIT.get_services();
+		_projectId = APIClientIntegrationTestSuiteIT.get_projectId();
 	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
+   @Test
+    public void UpdateScalarAttributeOnStory() throws MetaException, V1Exception{
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+        IAssetType storyType = _services.getMeta().getAssetType("Story");
+        Asset newStory = _services.createNew(storyType, _projectId);
+        IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+        String name = "Test Story " + _projectId + " Update scalar attribute";
+        newStory.setAttributeValue(nameAttribute, name);
+        _services.save(newStory);
 
+        assertNotNull(newStory.getOid());
+
+        Query query = new Query(newStory.getOid());
+        query.getSelection().add(nameAttribute);
+        Asset story = _services.retrieve(query).getAssets()[0];
+        String oldName = story.getAttribute(nameAttribute).getValue().toString();
+        story.setAttributeValue(nameAttribute, name + " - Name updated");
+        _services.save(story);
+
+       assertNotSame(oldName, story.getAttribute(nameAttribute).getValue().toString());
+    }
+
+   	@Test
+    public void UpdateSingleRelationAttributeOnStory() throws V1Exception {
+
+        IAssetType storyType = _services.getMeta().getAssetType("Story");
+        Asset newStory = _services.createNew(storyType, _projectId);
+        IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+        String name = "Test Story "+ _projectId+ " Update single-relation attribute";
+        newStory.setAttributeValue(nameAttribute, name);
+        _services.save(newStory);
+
+        assertNotNull(newStory.getOid());
+
+        IAttributeDefinition sourceAttribute = storyType.getAttributeDefinition("Source");
+        newStory.setAttributeValue(sourceAttribute, "StorySource:156");
+        _services.save(newStory);
+
+        Query query = new Query(newStory.getOid());
+        query.getSelection().add(sourceAttribute);
+        Asset story = _services.retrieve(query).getAssets()[0];
+
+        assertNotNull(story.getAttribute(sourceAttribute).getValue().toString());
+    }
+
+   	@Test
+    public void UpdateMultiRelationAttributeOnStory() throws V1Exception
+    {
+        IAssetType storyType = _services.getMeta().getAssetType("Story");
+        IAttributeDefinition nameAttribute = storyType.getAttributeDefinition("Name");
+
+        Asset parentStory = _services.createNew(storyType, _projectId);
+        String name = "Test Story " + _projectId +  " Update multi-relation attribute";
+        parentStory.setAttributeValue(nameAttribute,  name);
+        _services.save(parentStory);
+        
+        Asset child1Story = _services.createNew(storyType, _projectId);
+        child1Story.setAttributeValue(nameAttribute, name + " - Child 1");
+        _services.save(child1Story);
+        Asset child2Story = _services.createNew(storyType, _projectId);
+        child2Story.setAttributeValue(nameAttribute, name + " - Child 2");
+        _services.save(child2Story);
+
+        IAttributeDefinition dependantsAttribute = storyType.getAttributeDefinition("Dependants");
+        parentStory.addAttributeValue(dependantsAttribute, child1Story.getOid());
+        _services.save(parentStory);
+
+        Query query = new Query(parentStory.getOid());
+        query.getSelection().add(dependantsAttribute);
+        Asset story = _services.retrieve(query).getAssets()[0];
+
+       assertEquals(1, story.getAttribute(dependantsAttribute).getValues().length);
+
+        parentStory.addAttributeValue(dependantsAttribute, child2Story.getOid());
+        _services.save(parentStory);
+
+        Query query2 = new Query(parentStory.getOid());
+        query2.getSelection().add(dependantsAttribute);
+        story = _services.retrieve(query2).getAssets()[0];
+
+        assertEquals(2, story.getAttribute(dependantsAttribute).getValues().length);
+    }
 }
