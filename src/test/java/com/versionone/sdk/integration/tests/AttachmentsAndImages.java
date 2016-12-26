3681f8f75ad68072c4f82fa1b5945940ec9efb7b
valentin plechuc <vplechuc@gmail.com>
2016-12-26 09:34:53 -0300

    S-60792: removing unused imports

--- src/test/java/com/versionone/sdk/integration/tests/AttachmentsAndImages.java
+++ src/test/java/com/versionone/sdk/integration/tests/AttachmentsAndImages.java
@@ -2,7 +2,6 @@ package com.versionone.sdk.integration.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
